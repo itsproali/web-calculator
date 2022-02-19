14210d61e9938be0eaadea3863d1a5be8ca7cf1c
Mohammad Ali <itsproali@gmail.com>
2022-02-19 20:17:01 +0600

    All Function Added

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-document.getElementById('buttons').addEventListener('click', function (even) {
-    const myInput = even.target.innerText;
-    const previousItem = document.getElementById('preview');
-    previousItem.innerText = Number(previousItem.innerText) + Number(myInput);
-    console.log(previousItem.innerText);
-});
-document.getElementById('equal').addEventListener('click', function () {
-    document.getElementById('preview').value = '';
-})
+// Necessary HTML Tags or Buttons
+const preview = document.getElementById('preview');
+const result = document.getElementById('result');
+const buttons = document.querySelectorAll('button');
+
+// All Functions
+for (let button of buttons) {
+    button.addEventListener('click', function (e) {
+        let myInput = e.target.innerText;
+        if (myInput == 'AC') {
+            preview.innerText = '';
+            result.innerText = '';
+        } else if (myInput == '⌫') {
+            const previousNumber = preview.innerText;
+            preview.innerText = previousNumber.substring(0, previousNumber.length - 1);
+            result.innerText = '';
+        } else if (myInput == '%') {
+            alert('This is under construction!!! \n' +
+            'Please try divide option to get percentage');
+        } else if (myInput == '×') {
+            myInput = '*';
+            preview.innerText += '*';
+        } else if (myInput == '÷') {
+            myInput = '/';
+            preview.innerText += '/';
+        } else if (myInput == '=') {
+            var evalConvert = eval(preview.innerText);
+            result.innerText = evalConvert;
+        } else {
+            preview.innerText += myInput;
+        }
+    });
+};
